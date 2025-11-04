99321a87197bf59b9743f4bfd7f82d62547f6540
Matvey <rusinovmatvei@gmail.com>
2025-11-05 03:16:13 +0500

    added a renderer structure that will make html from tree (parsed tokens)

---
+++ cs/Markdown/Rendering/HtmlRenderer.cs
@@ -0,0 +1,22 @@
+using System.Text;
+using Markdown.Parsing.Nodes;
+
+namespace Markdown.Rendering;
+
+public class HtmlRenderer
+{
+    public string Render(RootNode document)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void RenderBlock(BlockNode block, StringBuilder sb)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void RenderInline(InlineNode node, StringBuilder sb)
+    {
+        throw new NotImplementedException();
+    }
+}
